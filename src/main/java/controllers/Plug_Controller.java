41a9e5e15068f91ae737e372c7d2969b3c2560fc
vera_kareeva <vera-0508@yandex.ru>
2025-05-30 15:56:23 +0300

    response times added

--- src/main/java/controllers/Plug_Controller.java
+++ src/main/java/controllers/Plug_Controller.java
@@ -6,17 +6,24 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/plug")
 public class Plug_Controller {
-    @GetMapping
+    @GetMapping (produces = "application/json")
     public String getStaticJson() {
+        try {
+            Thread.sleep(1000 );
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         return "{\"login\":\"Login1\",\"status\":\"ok\"}";
     }
 
     @PostMapping(consumes = "application/json", produces = "application/json")
     public Map<String, String> postLogin(@RequestBody Map<String, String> input) {
+
         String login = input.get("login");
         String password = input.get("password");
         String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
@@ -25,6 +32,12 @@ public class Plug_Controller {
         response.put("login", login);
         response.put("password", password);
         response.put("date", date);
+
+        try {
+            Thread.sleep(1000 + new Random().nextInt(1001));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         return response;
     }
 }
