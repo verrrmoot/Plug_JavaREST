03a491f7557aac1f6884237ec54052f6b5af9d8d
Vera Kareeva <vera-0508@yandex.ru>
2025-06-06 13:57:26 +0300

    Lombok annotations added

--- src/main/java/api/v1/models/User.java
+++ src/main/java/api/v1/models/User.java
@@ -2,41 +2,17 @@ package api.v1.models;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
 public class User {
+    @NonNull
     @NotBlank
     private String login;
+    @NonNull
     @NotBlank
     private String password;
     private String date;
 
-    public User(String login, String password){
-        this.login = login;
-        this.password = password;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
