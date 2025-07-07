0a0814b3b94543afb40642ab1f014ef05641bb98
Vera Kareeva <vera-0508@yandex.ru>
2025-07-07 16:13:27 +0300

    corrections added

--- src/main/java/api/v1/dao/DataBaseWorker.java
+++ src/main/java/api/v1/dao/DataBaseWorker.java
@@ -15,76 +15,51 @@ public class DataBaseWorker {
     private static final String PASSWORD = "verrrmoot";
 
     public User select(String login) throws SelectException {
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
+        String query = "SELECT * FROM users_auth u JOIN users_emails e " +
+                        "ON u.login = e.login WHERE u.login = '" + login + "'";
         User user = null;
-        try{
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("SELECT * FROM users_auth u JOIN users_emails e " +
-                    "ON u.login = e.login " +
-                    "WHERE u.login = '" + login + "'");
-            if (resultSet.next()){
-                user = new User(
+
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+             PreparedStatement pstatement = connection.prepareStatement(query);
+             ResultSet resultSet = pstatement.executeQuery())
+            {
+                if (resultSet.next()){
+                    user = new User(
                         resultSet.getString("login"),
                         resultSet.getString("password"),
                         resultSet.getString("email"),
                         new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(resultSet.getTimestamp("date").getTime()))
                 );
-            } else if (user == null) {
-                throw new SelectException("User " + login + " not found");
+                } else if (user == null) {
+                    throw new SelectException("User " + login + " not found");
+                }
+                return user;
             }
-        }
         catch (SQLException e){
-            System.out.println("Error with connection " + e.getMessage());
-            e.printStackTrace();
+            throw new SelectException("Error with select " + e.getMessage());
         }
-        finally {
-            try {
-                if (connection != null) connection.close();
-                if (statement != null) statement.close();
-                if (resultSet != null) resultSet.close();
-            }
-            catch (SQLException e){
-                System.out.println("error with close connection" + e.getMessage());
-            }
-        }
-        return user;
     }
 
     public int insert(User user) throws SelectException {
-        String sqlauth = "INSERT INTO users_auth VALUES (?, ?, ?)";
-        String sqlemail = "INSERT INTO users_emails VALUES (?, ?)";
+        String sqlquery = "INSERT INTO users_auth VALUES (?, ?, ?); \n" +
+                "INSERT INTO users_emails VALUES (?, ?)";
         int inserts = 0;
 
-        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)){
-            connection.setAutoCommit(false);
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        PreparedStatement pstatement = connection.prepareStatement(sqlquery)){
 
-            try (PreparedStatement psauth = connection.prepareStatement(sqlauth);
-                PreparedStatement psemail = connection.prepareStatement(sqlemail)){
+            pstatement.setString(1, user.getLogin());
+            pstatement.setString(2, user.getPassword());
+            pstatement.setTimestamp(3, Timestamp.valueOf(user.getDate()));
 
-                psauth.setString(1, user.getLogin());
-                psauth.setString(2, user.getPassword());
-                psauth.setTimestamp(3, Timestamp.valueOf(user.getDate()));
-                inserts += psauth.executeUpdate();
-
-                psemail.setString(1, user.getEmail());
-                psemail.setString(2, user.getLogin());
-                inserts += psemail.executeUpdate();
-
-                connection.commit();
-            }catch (SQLException e){
-                connection.rollback();
-                System.out.println("Error with insert " + e.getMessage());
-                inserts = 0;
-                throw new SelectException("Error with insert " + e.getMessage());
-            }
+            pstatement.setString(4, user.getEmail());
+            pstatement.setString(5, user.getLogin());
+            inserts += pstatement.executeUpdate();
+            return inserts;
         }
         catch (SQLException e){
-            System.out.println("Error connection " + e.getMessage());
+            throw new SelectException("Error with insert " + e.getMessage());
         }
-        return inserts;
     }
 
 }
